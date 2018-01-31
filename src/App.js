6c95bb61e154b1973343588d4f213e651ae62e87
Zhang Weijie <email@zhangweijie.cn>
2018-01-31 19:37:20 +0800

    Support 1024x1024

--- src/App.js
+++ src/App.js
@@ -1,88 +1,173 @@
-import React, { Component } from 'react'
-import Result from './Result'
+import React, { Component } from 'react';
+import Result from './Result';
 import {
-  expandShortLink,
-  searchAppById,
-  searchIosApp,
-  searchMacApp
- } from './iTunes'
-import search from './search.svg'
-import './App.css'
+    expandShortLink,
+    searchAppById,
+    searchIosApp,
+    searchMacApp,
+} from './iTunes';
+import search from './search.svg';
+import './App.css';
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      input: '',
-      results: [],
-      country: 'US'
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            results: [],
+            country: 'US',
+            resolution: 512,
+        };
+        this.search = this.search.bind(this);
     }
-    this.search = this.search.bind(this)
-  }
 
-  async search () {
-    let { input, country } = this.state
-    input = input.trim()
-    let url = input
-    const itunesReg = /^(http|https):\/\/itunes/
-    const idReg = /\/id(\d+)/i
-    const shortReg = /^(http|https):\/\/appsto/
+    async search() {
+        let { input, country } = this.state;
+        input = input.trim();
+        let url = input;
+        const itunesReg = /^(http|https):\/\/itunes/;
+        const idReg = /\/id(\d+)/i;
+        const shortReg = /^(http|https):\/\/appsto/;
 
-    try {
-      if (shortReg.test(input)) {
-        url = await expandShortLink(input)
-      }
-      if (itunesReg.test(url) && idReg.test(url)) {
-        const id = idReg.exec(url)[1]
-        const data = await searchAppById(id, country)
-        this.setState({ results: data.results })
-      } else {
-        const data = await Promise.all([searchIosApp(input, country), searchMacApp(input, country)])
-        this.setState({ results: data[0].results.concat(data[1].results) })
-      }
-    } catch (err) {
-      console.error(err)
+        try {
+            if (shortReg.test(input)) {
+                url = await expandShortLink(input);
+            }
+            if (itunesReg.test(url) && idReg.test(url)) {
+                const id = idReg.exec(url)[1];
+                const data = await searchAppById(id, country);
+                this.setState({ results: data.results });
+            } else {
+                const data = await Promise.all([
+                    searchIosApp(input, country),
+                    searchMacApp(input, country),
+                ]);
+                this.setState({
+                    results: data[0].results.concat(data[1].results),
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
-  }
 
-  render () {
-    const { input, results, country } = this.state
-    return (
-      <div className='app'>
-        <header>
-          <div className='center'>
-            <div className='logo'>HQ ICON</div>
-            <div className='description'>Get high quality icons from App Store</div>
-            <div className="country">
-                Country:
-                <lable onClick={() => this.setState({country: 'US'})}><input name="store" type="radio" checked={country === 'US'} />US 🇺🇸</lable>
-                <lable onClick={() => this.setState({country: 'CN'})}><input name="store" type="radio" checked={country === 'CN'} />CN 🇨🇳</lable>
-                <lable onClick={() => this.setState({country: 'JP'})}><input name="store" type="radio" checked={country === 'JP'} />JP 🇯🇵</lable>
+    render() {
+        const { input, results, country, resolution } = this.state;
+        return (
+            <div className="app">
+                <header>
+                    <div className="center">
+                        <div className="logo">HQ ICON</div>
+                        <div className="description">
+                            Get high quality icons from App Store
+                        </div>
+                        <div className="options">
+                            Country:
+                            <lable
+                                onClick={() => this.setState({ country: 'US' })}
+                            >
+                                <input
+                                    name="store"
+                                    type="radio"
+                                    checked={country === 'US'}
+                                />US 🇺🇸
+                            </lable>
+                            <lable
+                                onClick={() => this.setState({ country: 'CN' })}
+                            >
+                                <input
+                                    name="store"
+                                    type="radio"
+                                    checked={country === 'CN'}
+                                />CN 🇨🇳
+                            </lable>
+                            <lable
+                                onClick={() => this.setState({ country: 'JP' })}
+                            >
+                                <input
+                                    name="store"
+                                    type="radio"
+                                    checked={country === 'JP'}
+                                />JP 🇯🇵
+                            </lable>
+                        </div>
+                        <div className="options">
+                            Image Resolution:
+                            <lable
+                                onClick={() =>
+                                    this.setState({ resolution: 512 })
+                                }
+                            >
+                                <input
+                                    name="resolution"
+                                    type="radio"
+                                    checked={resolution === 512}
+                                />
+                                512x512
+                            </lable>
+                            <lable
+                                onClick={() =>
+                                    this.setState({ resolution: 1024 })
+                                }
+                            >
+                                <input
+                                    name="resolution"
+                                    type="radio"
+                                    checked={resolution === 1024}
+                                />
+                                1024x1024
+                            </lable>
+                        </div>
+                        <div className="search">
+                            <input
+                                className="search-input"
+                                placeholder="iTunes url or App name"
+                                value={input}
+                                onChange={(e) =>
+                                    this.setState({ input: e.target.value })
+                                }
+                                onKeyDown={(e) =>
+                                    e.keyCode === 13 ? this.search() : ''
+                                }
+                            />
+                            <div
+                                className="search-button"
+                                onClick={this.search}
+                            >
+                                <img
+                                    src={search}
+                                    className="search-icon"
+                                    alt="search"
+                                />
+                            </div>
+                        </div>
+                        <a
+                            className="github-button"
+                            href="https://github.com/zhangweijie-cn/hq-icon"
+                            data-style="mega"
+                            aria-label="Star zhangweijie-cn/hq-icon on GitHub"
+                        >
+                            Star
+                        </a>
+                    </div>
+                </header>
+                <main className="results">
+                    {results.map((result) => (
+                        <Result
+                            key={result.trackId}
+                            data={result}
+                            resolution={resolution}
+                        />
+                    ))}
+                </main>
+                <footer className="footer">
+                    Created by @<a href="https://github.com/zhangweijie-cn">
+                        zhangweijie-cn
+                    </a>
+                </footer>
             </div>
-            <div className='search'>
-              <input
-                className='search-input'
-                placeholder='iTunes url or App name'
-                value={input}
-                onChange={(e) => this.setState({ input: e.target.value })}
-                onKeyDown={(e) => e.keyCode === 13 ? this.search() : ''}
-              />
-              <div className='search-button' onClick={this.search}>
-                <img src={search} className='search-icon' alt='search' />
-              </div>
-            </div>
-            <a className="github-button" href="https://github.com/zhangweijie-cn/hq-icon" data-style="mega" aria-label="Star zhangweijie-cn/hq-icon on GitHub">Star</a>
-          </div>
-        </header>
-        <main className='results'>
-          {
-            results.map(result => <Result key={result.trackId} data={result} />)
-          }
-        </main>
-        <footer className='footer'>Created by @<a href='https://github.com/zhangweijie-cn'>zhangweijie-cn</a></footer>
-      </div>
-    )
-  }
+        );
+    }
 }
 
-export default App
+export default App;
--- src/Result.js
+++ src/Result.js
@@ -1,43 +1,53 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import drawOutline from './drawOutline'
-import './Result.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import drawOutline from './drawOutline';
+import './Result.css';
 
 class Result extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      base64: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            base64: '',
+        };
     }
-  }
 
-  async componentDidMount () {
-    const { data } = this.props
-    const base64 = await drawOutline(data)
-    this.setState({
-      base64
-    })
-  }
+    async componentDidMount() {
+        const { data, resolution } = this.props;
+        const base64 = await drawOutline(data, resolution);
+        this.setState({
+            base64,
+        });
+    }
 
-  render () {
-    const { data } = this.props
-    const { trackName, kind } = data
-    const { base64 } = this.state
-    const platform = kind.startsWith('mac') ? 'mac' : 'iOS'
-    return (
-      <div className='result'>
-        <a href={base64} download={`${trackName}-${platform}.png`}>
-          <img className='icon' src={base64} alt={trackName} />
-        </a>
-        <div className='kind'>{platform}</div>
-        {trackName}
-      </div>
-    )
-  }
+    async componentWillReceiveProps(nextProps) {
+        if (nextProps.resolution !== this.props.resolution) {
+            const { data, resolution } = nextProps;
+            const base64 = await drawOutline(data, resolution);
+            this.setState({
+                base64,
+            });
+        }
+    }
+
+    render() {
+        const { data, resolution } = this.props;
+        const { trackName, kind } = data;
+        const { base64 } = this.state;
+        const platform = kind.startsWith('mac') ? 'mac' : 'iOS';
+        return (
+            <div className="result">
+                <a href={base64} download={`${trackName}-${platform}-${resolution}x${resolution}.png`}>
+                    <img className="icon" src={base64} alt={trackName} />
+                </a>
+                <div className="kind">{platform}</div>
+                {trackName}
+            </div>
+        );
+    }
 }
 
 Result.propTypes = {
-  data: PropTypes.object.isRequired
-}
+    data: PropTypes.object.isRequired,
+};
 
-export default Result
+export default Result;
